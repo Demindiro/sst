7e60e4f1b62bcaf5a6855b92b973f5d5052e3cc7
David Hoppenbrouwers <david@demindiro.com>
2019-09-10 15:45:36 +0200

    Added some debug info in vasm_syscall

--- src/interpreter/syscall.c
+++ src/interpreter/syscall.c
@@ -31,18 +31,26 @@ void vasm_syscall(int64_t regs[32], uint8_t *mem) {
 		addr.sin6_family = AF_INET6;
 		addr.sin6_port   = htons(regs[2]);
 		memcpy(&addr.sin6_addr, (void *)(mem + regs[3]), 16);
+#ifndef NDEBUG
+		char buf[64];
+		inet_ntop(AF_INET6, &addr.sin6_addr, buf, sizeof buf);
+#endif
 		if (bind(fd, (struct sockaddr *)&addr, sizeof addr) < 0) {
 			close(fd);
+			DEBUG("listen(\"%s\", %u) = -1", buf, addr.sin6_port);
 			goto _default;
 		}
 		if (listen(fd, 5) < 0) {
 			close(fd);
+			DEBUG("listen(\"%s\", %u) = -1", buf, addr.sin6_port);
 			goto _default;
 		}
+		DEBUG("listen(\"%s\", %u) = %d", buf, addr.sin6_port, fd);
 		regs[0] = fd;
 		break;
 	case 5: // accept(fd)
 		regs[0] = accept(regs[1], NULL, NULL);
+		DEBUG("accept(%ld) = %ld", regs[1], regs[0]);
 		break;
 	case 9: // signal
 		switch (regs[1]) {
